29bd59d2d6f2a510c67a83bdf06dcc128acf858a
Ruvimal <ruvimal@gmail.com>
2016-07-26 22:21:49 +0530

    Ruvimal 1st

--- Asia/Asia/Login.Designer.cs
+++ Asia/Asia/Login.Designer.cs
@@ -33,7 +33,6 @@
             this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.metroButton1 = new MetroFramework.Controls.MetroButton();
-            this.metroButton2 = new MetroFramework.Controls.MetroButton();
             this.SuspendLayout();
             // 
             // metroLabel1
@@ -127,21 +126,11 @@
             this.metroButton1.Text = "Login";
             this.metroButton1.UseSelectable = true;
             // 
-            // metroButton2
-            // 
-            this.metroButton2.Location = new System.Drawing.Point(155, 167);
-            this.metroButton2.Name = "metroButton2";
-            this.metroButton2.Size = new System.Drawing.Size(75, 23);
-            this.metroButton2.TabIndex = 5;
-            this.metroButton2.Text = "Cancel";
-            this.metroButton2.UseSelectable = true;
-            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(332, 281);
-            this.Controls.Add(this.metroButton2);
             this.Controls.Add(this.metroButton1);
             this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroTextBox2);
@@ -162,7 +151,6 @@
         private MetroFramework.Controls.MetroTextBox metroTextBox2;
         private MetroFramework.Controls.MetroLabel metroLabel2;
         private MetroFramework.Controls.MetroButton metroButton1;
-        private MetroFramework.Controls.MetroButton metroButton2;
     }
 }
 
