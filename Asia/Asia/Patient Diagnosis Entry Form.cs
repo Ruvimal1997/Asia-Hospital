eaab581147f5f491817b14506b4f05252f6b9329
piushan1992 <prasanga.shan92@gmail.com>
2016-07-27 01:53:06 +0530

    1.51AM  Patient Diagnosis Entry Form

---
+++ Asia/Asia/Patient Diagnosis Entry Form.cs
@@ -0,0 +1,26 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asia
+{
+    public partial class Patient_Diagnosis_Entry_Form : MetroForm
+    {
+        public Patient_Diagnosis_Entry_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Patient_Diagnosis_Entry_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
