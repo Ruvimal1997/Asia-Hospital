a4c6560f9c71419b4318490cf219494ef2359342
piushan1992 <prasanga.shan92@gmail.com>
2016-07-27 03:54:52 +0530

    3.54AM Patient Receipt Entry

---
+++ Asia/Asia/Patient Receipt Entry.cs
@@ -0,0 +1,26 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asia
+{
+    public partial class Patient_Receipt_Entry : MetroForm
+    {
+        public Patient_Receipt_Entry()
+        {
+            InitializeComponent();
+        }
+
+        private void Patient_Receipt_Entry_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
